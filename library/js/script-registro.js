301912ecf7ff85ffdc8e3b6d4986404b5f96e970
Alonso Avila <avila@clicker360.com>
2013-08-28 07:35:20 -0500

    Mensaje de activacion en registro estilo en pagina correcion de status en paginas de front

--- library/js/script-registro.js
+++ library/js/script-registro.js
@@ -65,8 +65,25 @@ var registros = (function() {
             var post = $(this).serialize();
             Ajax.post('/r/wsr/put', post, function(response){
                    if(response.status == 'ok'){
-                        $('tusdatos').append('');
-                           alert('registrado correctamente');
+                        $('#tusdatos').html(
+                            '<h2>Gracias por registrarte en "El buen Fin"</h2>'
+                            + '<p><strong>Estás a un paso de activar tu cuenta y comenzar con el registro de tu empresa</strong></p>'
+                            + '<p><strong>En un momento recibiras un correo electrónico con tus datos de acceso</strong></p>'
+                            + '<div class="row"><div class="span6">'
+                            + '<dl class="dl-horizontal">'
+                            + '<dt>Usuario:</dt><dd>' + response.Nombre + '</dd>'
+                            + '<dt>Apellido:</dt><dd>' + response.Apellidos + '</dd>'
+                            + '<dt>Correo:</dt><dd>' + response.Email + '</dd>'
+                            + '<dt>Contraseña:</dt><dd>' + response.Pass + '</dd>'
+                            + '</dl>'
+                            + '</div>'
+                            + '<div class="span6">'
+                            + '<ul><li>Ingresa a tu cuenta de correo</li><li>Abre el correo que te enviamos</li><li>Haz click en la liga que aparece en él</li><li>Cpmienza el registro de tu(s) empresa(s)</li></ul>'
+                            + '<p><strong>Es importante que actives antes de 15 días ya que la activación expira. Si expira tu código o no es válido puedes realizar tu registro nuevamente</strong></p>'
+                            + '</div></div>'
+                            );
+                       Ajax.hidePreload($('#registro-enviado-block'));
+                           //alert('registrado correctamente');
                    }
            });
         })
@@ -76,17 +93,17 @@ var registros = (function() {
             event.preventDefault();
             var post = $(this).serialize();
             Ajax.post('/r/wsr/post', post, function(response){
-                   if(response.status == 'ok'){
-                        $('tusdatos').append('');
-                           alert('registrado correctamente');
-                   }
+                  // if(response.status == 'ok'){
+
+                           //alert('registrado correctamente');
+                 //  }
            });
         })
     };
     var consultaregistro = function() {
         Ajax.get('/r/wsr/get', function(response) {
             $('#actualizaregistro').formParams(response);
-            Ajax.hidePreload($('#empresas-detalle'));
+
         });
     };
     return {
