7d422cd632394037f98fce78e611d390cde8e303
Alonso Avila <avila@clicker360.com>
2013-08-27 19:48:42 -0500

    Funcionamiento del app Dashboard, cargas de datos por default con ajax. Funcionamiento de formulario de registro

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,117 +1,172 @@
-(function(){
+(function() {
+     var initSucursales = function() {
+       $(document).on('click','a.ver-sucursales', function(event) {
+           event.preventDefault();
+           var rel = $(this).attr('rel');
 
-/*
-  var verSucusales = function(){
-    $('.ver-sucursales').on('click', function(event){
-      //event.preventDefault();
-      //event.stopPropagation();
-      var rel = $(this).attr('rel');
-      sucursales.initSucursales(rel);
-    });
-  }
+           sucursales.initSucursales(rel);
+       });
+   };
+   var llenaformempresas = function() {
+       $(document).on('click','a.editar-empresa', function(event) {
+           event.preventDefault();
+           var rel = $(this).attr('rel');
+           empresas.empresaformdesdejson(rel);
+       });
+   };
+   var llenaformsucursal = function() {
+       $(document).on('click','a.editar-sucursal', function(event) {
+           event.preventDefault();
+           var rel = $(this).attr('rel');
+           sucursales.sucursalformdesdejson(rel);
+       });
+   };
+    var initEmpresas = function() {
+        empresas.initEmpresas(); //lista de empresas
+    };
+    var registrarse = function () {
+        registros.registrarse();
+    };
+    var execute = function() {
+        $(document).ready(function() {
+            //verSucusales2();
+            registrarse();
+            llenaformempresas();
+            llenaformsucursal();
+            initEmpresas();
+            initSucursales();
+        });
+    };
+    return execute();
+})();
+var Ajax = (function() {
+    var _showPreload = function() {
+        $('#preloader').fadeIn('fast');
+    };
+    var hidePreload = function(bloque) {
+        $('#preloader').fadeOut('fast');
+        if(typeof bloque !== 'undefined'){
+            $('.active').removeClass('active').addClass('inactive');
+            bloque.addClass('active').removeClass('inactive');   
+        }
+        
 
-   var verSucusales2 = function() {
-    $('.ver-sucursales').click(function(){
-      var rel = $(this).attr('rel');
-      sucursales.initSucursales(rel);
-    });
-   }
-
-*/
-
- var initSucursales = function(){
-    //sucursales.initSucursales(); // lista de sucursales
-    sucursales.sucursalformdesdejson(); // llenar formulario de sucursales
-  }
-
-  var initEmpresas = function(){
-    //empresas.initEmpresas(); //lista de empresas
-    empresas.empresaformdesdejson(); // llenar formulario de empresas
-  }
-  var execute = function() {
-    $(document).ready(function(){
-        //verSucusales2();
-        initEmpresas();
-        initSucursales();
-    });
-  }
-  return execute();
+    };
+    var get = function(url, callback) {
+        _showPreload();
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            success: function(response) {
+                if (typeof callback === 'function') {
+                    callback.call(callback, response);
+                }
+            }
+        });
+    };
+    var post = function(url, data, callback) {
+        _showPreload();
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            data: data,
+            success: function(response) {
+                if (typeof callback === 'function') {
+                    callback.call(callback, response);
+                }
+            }
+        });
+    };
+    return {
+        hidePreload: hidePreload,
+        get: get,
+        post: post
+    };
 })();
 
-
-var empresas = (function(){
-  //listas desde
-  var initEmpresas = function(){
-    var empresasArray = {},
-        imprimeTemplate = '',
-        underTemplateID = $('#empresaTemplate').html(),
-        underTemplate = _.template(underTemplateID);        
-        $.ajax({
-          url: '/r/wse/gets',
-          async: false,
-          dataType: 'json',
-          success: function(json) {
-            empresasArray = json;
-          }
+var empresas = (function() {
+    var initEmpresas = function() {
+        var imprimeTemplateDash = '',
+            underTemplateIDash = $('#empresaTemplateDash').html(),
+            underTemplateDash = _.template(underTemplateIDash);
+        var imprimeTemplate = '',
+                underTemplateID = $('#empresaTemplate').html(),
+                underTemplate = _.template(underTemplateID);
+        Ajax.get('/r/wse/gets', function(response) {
+            imprimeTemplateDash = underTemplateDash({
+                empresasArrayDash: response
+            });
+            imprimeTemplate = underTemplate({
+                empresasArray: response
+            });
+            $('#empresasBlockDash').html(imprimeTemplateDash);
+            $("#empresasBlock").html(imprimeTemplate);
+            Ajax.hidePreload();
         });
-        imprimeTemplate = underTemplate({ empresasArray:empresasArray });
-        $("#empresasBlock").html(imprimeTemplate);
-  }
-  //llenar formulario desde el json
-  var empresaformdesdejson  = function(){
-   //var forma = $('#sucursal-form');
-    var datosform = {}
-        $.ajax({
-          url: '/r/wse/get?IdEmp=ecusboweznoeuqemhrna',
-          async: false,
-          dataType: 'json',
-          success: function(json) {
-            datosform = json;
-          }
+    };
+    var empresaformdesdejson = function(codeRel) {
+        Ajax.get('/r/wse/get?IdEmp=' + codeRel, function(response) {
+            $('#empresa-form').formParams(response, true);
         });
-    $('#empresa-form').formParams(datosform, true);
-  }
-  return {
-    initEmpresas:initEmpresas,
-    empresaformdesdejson:empresaformdesdejson
-  }
+    };
+    return {
+        initEmpresas: initEmpresas,
+        empresaformdesdejson: empresaformdesdejson
+    };
 })();
 
-var sucursales = (function(){
-  //llenar las listas desde el json
-  var initSucursales = function(){
-    var sucursalesArray = {},
-        imprimeTemplate = '',
-        underTemplateID = $('#sucursalesTemplate').html(),
-        underTemplate = _.template(underTemplateID);        
-        $.ajax({
-          url: '/r/wss/gets?IdEmp=ghjkhxiwknbpvftwdrsm', 
+var sucursales = (function() {
+    var initSucursales = function(codeRel) {
+        //var imprimeTemplate = '',
+        var sucursalesArray = {},
+             imprimeTemplate = '',
+                underTemplateID = $('#sucursalesTemplate').html(),
+                underTemplate = _.template(underTemplateID);
+        Ajax.get('/r/wss/gets?IdEmp=' + codeRel, function(response) {
+            imprimeTemplate = underTemplate({sucursalesArray: response});
+            $("#sucursalesBlock").html(imprimeTemplate);
+            Ajax.hidePreload($('#sucursales-lista'));
+        });
+    };
 
-          async: false,
-          dataType: 'json',
-          success: function(json) {
-            sucursalesArray = json;
-          }
+
+    var sucursalformdesdejson = function(codeRel) {
+        Ajax.get('/r/wss/get?IdSuc=' + codeRel, function(response) {
+            $('#sucursal-form').formParams(response, true);
+              Ajax.hidePreload($('#sucursal-detalle'));
         });
-        imprimeTemplate = underTemplate({ sucursalesArray:sucursalesArray });
-        $("#sucursalesBlock").html(imprimeTemplate);
-  }
-  // llenar el formulario desde el json
-  var sucursalformdesdejson  = function(){ 
-   //var forma = $('#sucursal-form');
-    var datosform = {}
-        $.ajax({
-          url: '/r/wss/get?IdSuc=fbpchxospboklwmfiult',
-          async: false,
-          dataType: 'json',
-          success: function(json) {
-            datosform = json;
-          }
-        });
-    $('#sucursal-form').formParams(datosform, true);
-  }
-  return {
-    initSucursales:initSucursales,
-    sucursalformdesdejson:sucursalformdesdejson
-  }
+    };
+
+    var registro = function () {
+        $('form#registroform').on('submit', function(event){
+            event.preventDefault();
+            var post = $(this).serialize();
+            Ajax.post('/r/wsr/put', post, function(response){
+                   if(response.success){
+                           alert('registrado correctamente');
+                   }
+           });
+        })
+    };
+    return {
+        initSucursales: initSucursales,
+        sucursalformdesdejson: sucursalformdesdejson,        
+    };
+})();
+
+var registros = (function() {
+    var registrarse = function () {
+        $('form#registroform').on('submit', function(event){
+            event.preventDefault();
+            var post = $(this).serialize();
+            Ajax.post('/r/wsr/put', post, function(response){
+                   if(response.success){
+                           alert('registrado correctamente');
+                   }
+           });
+        })
+    };
+    return {
+        registrarse: registrarse
+    };
 })();
